500d7bcd290851babc43f795641cc75d0bcb5c80
postam95 <postam95@gmail.com>
2023-09-19 13:27:03 +0200

    Update pointers_in_c.c

--- c_lang/pointers_in_c.c
+++ c_lang/pointers_in_c.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+// Updates the value of the memory address a and b with sum and absolute difference of a and b.
 void update(int *a,int *b) {
     int sum = *a + *b;
     int abs_diff = *a > *b ? *a - *b : *b - *a;
